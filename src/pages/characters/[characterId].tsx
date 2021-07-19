711a7879cda7906d3092454cca38592c004265f1
HerbertSousa <herberti1405@outlook.com>
2021-07-18 23:02:44 -0300

    show character info with comics and get infos of series and events

--- src/pages/characters/[characterId].tsx
+++ src/pages/characters/[characterId].tsx
@@ -1,7 +1,12 @@
-import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next"
+import { Button, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from "@material-ui/core";
+import { GetStaticPaths, GetStaticProps } from "next"
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { api } from "../../services/api";
+
+import { useStyles } from './styles';
 
 type Comic = {
     id: number;
@@ -11,11 +16,17 @@ type Comic = {
 }
 
 type Serie = {
-    name: string;
+    id: number;
+    title: string;
+    thumbnail: string;
+    details: string;
 }
 
-type Story = {
-    name: string;
+type Event = {
+    id: number;
+    title: string;
+    thumbnail: string;
+    details: string;
 }
 
 interface CharacterProps {
@@ -26,16 +37,67 @@ interface CharacterProps {
         thumbnail: string;
         comics: Comic[];
         series: Serie[];
-        stories: Story[];
+        events: Event[];
     }
 }
 
 export default function Character({ character }: CharacterProps) {
+    const classes = useStyles();
+
     return (
         <>
             <Head>
                 <title>{ character.name } | Marvel</title>
             </Head>
+            <Container className={classes.root}>
+                <Grid container spacing={5} className={classes.header}>
+                    <Grid item md={3}>
+                        <Image src={character.thumbnail} width="300" height="450" alt={character.name} />
+                    </Grid>
+                    <Grid item md={9}>
+                        <Typography variant="h1" noWrap>
+                            {character.name}
+                        </Typography>
+                        <Typography variant="body1">
+                            {character.description}
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={5} className={classes.comic}>
+                    <Grid xs={12}>
+                        <Typography variant="h2" noWrap>
+                            Comics
+                        </Typography>
+                    </Grid>
+                    <Grid container spacing={4} className={classes.cardList}>
+                        {character.comics.map((comic) => (
+                            <Grid key={comic.id} item xs={2}>
+                                <Card className={classes.card}>
+                                    <div>
+                                        <CardMedia>
+                                            <Image src={comic.thumbnail} width="300" height="450" alt={comic.title} />
+                                        </CardMedia>
+                                    </div>
+                                    <div>
+                                        <CardContent className={classes.cardDescription}>
+                                            <Typography variant="body2">
+                                                {comic.title}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Link href={comic.details} passHref={true}>
+                                            <Button variant="contained" color="secondary" className={classes.cardButton}>Learn More</Button>
+                                            </Link>
+                                        </CardActions>
+                                    </div>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
+                
+            </Container>
+
         </>
     )
 }
@@ -54,25 +116,45 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
 
     const findCharacter = await api.get(`/characters/${characterId}?ts=stringgrande5&apikey=7bef0f712b879922a50fccaee6d20219&hash=85f5fed85ba5469fe2e7f2d0c6320874`);
     const findCharacterComic = await api.get(`/characters/${characterId}/comics?ts=stringgrande5&apikey=7bef0f712b879922a50fccaee6d20219&hash=85f5fed85ba5469fe2e7f2d0c6320874`);
+    const findCharacterSerie = await api.get(`/characters/${characterId}/series?ts=stringgrande5&apikey=7bef0f712b879922a50fccaee6d20219&hash=85f5fed85ba5469fe2e7f2d0c6320874`);
+    const findCharacterEvent = await api.get(`/characters/${characterId}/events?ts=stringgrande5&apikey=7bef0f712b879922a50fccaee6d20219&hash=85f5fed85ba5469fe2e7f2d0c6320874`);
+
+
 
     const getCharacter = findCharacter.data.data.results[0]
     const getCharacterComic = findCharacterComic.data.data.results
+    const getCharacterSerie = findCharacterSerie.data.data.results
+    const getCharacterEvent = findCharacterEvent.data.data.results
 
     const character = {
         id: getCharacter.id,
         name: getCharacter.name,
         description: getCharacter.description,
-        thumbnail: `${getCharacter.path}/portrait_uncanny.${getCharacter.extension}`,
+        thumbnail: `${getCharacter.thumbnail.path}/portrait_uncanny.${getCharacter.thumbnail.extension}`,
         comics: getCharacterComic.map(comic => {
             return {
                 id: comic.id,
                 title: comic.title,
-                thumbnail: `${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}`,
+                thumbnail: `${comic.thumbnail.path}/portrait_xlarge.${comic.thumbnail.extension}`,
                 details: comic.urls[0].url,
             }
         }),
-        series: {},
-        stories: {},
+        series: getCharacterSerie.map(serie => {
+            return {
+                id: serie.id,
+                title: serie.title,
+                thumbnail: `${serie.thumbnail.path}/portrait_xlarge.${serie.thumbnail.extension}`,
+                details: serie.urls[0].url
+            }
+        }),
+        events: getCharacterEvent.map(event => {
+            return {
+                id: event.id,
+                title: event.title,
+                thumbnail: `${event.thumbnail.path}/portrait_xlarge.${event.thumbnail.extension}`,
+                details: event.urls[0].url
+            }
+        }),
     };
 
     return { 
